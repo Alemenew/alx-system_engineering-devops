fb91b719f67f8c53cf03c36e350ecdab88cd8d4e
alemenew <alemenewendawek@gmail.com>
2022-10-20 01:32:38 +0300

    fix bug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Using https://jsonplaceholder.typicode.com
-gathers data from API and exports it to CSV file
+returns info about employee TODO progress
 Implemented using recursion
 """
 import re
@@ -19,15 +19,15 @@ if __name__ == '__main__':
             id = int(sys.argv[1])
             user_res = requests.get('{}/users/{}'.format(API, id)).json()
             todos_res = requests.get('{}/todos'.format(API)).json()
-            user_name = user_res.get('username')
+            user_name = user_res.get('name')
             todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            with open('{}.csv'.format(id), 'w') as file:
-                for todo in todos:
-                    file.write(
-                        '"{}","{}","{}","{}"\n'.format(
-                            id,
-                            user_name,
-                            todo.get('completed'),
-                            todo.get('title')
-                        )
-                    )
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
